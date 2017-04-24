e40f28f4d686ae2589b66837de07e3d4ad961665
Simon Chiu <simonchiu@hotmail.co.uk>
2017-04-24 22:12:51 +0100

    Added sound manager with music playing

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager instance = null;
+    public AudioSource efxSource;
+    public AudioSource musicSource;
+
+
+
+	// Use this for initialization
+	void Awake ()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+	}
+
+    public void PlaySingle(AudioClip clip)
+    {
+        efxSource.clip = clip;
+        efxSource.loop = false;
+        efxSource.Play();
+    }
+
+    public void PlayLoop(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
