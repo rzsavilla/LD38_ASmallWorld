726eb6713c84506424d5e8e7b651e958a5494761
rzsavilla <rzsavilla@googlemail.com>
2017-04-24 23:00:33 +0100

    Implemented Menu navigation
    Start Screen
    How to play screen
    Game Screen

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+	// Use this for initialization
+    public void GoToScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
